73db3163e4979f3cbdd4a5209271ba4af4ebe488
anisul islam <anisulislam980340@gmail.com>
2023-09-15 12:30:33 +0600

    make palindrome solved

---
+++ Make_Palindrome.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string a;
+        cin>>a;
+        string b;
+        b=a;
+        reverse(b.begin(), b.end());
+
+        int n=a.size();
+        int m= b.size();
+        int dp[n+1][m+1];
+
+        for(int i=0; i<= n; i++){
+            for(int j=0; j<= m; j++){
+                if(i==0 || j==0) dp[i][j]=0;
+            }
+        }
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<= m; j++){
+                if(a[i-1]==b[j-1]){
+                    dp[i][j]= dp[i-1][j-1]+1;
+                }
+                else{
+                    int op1= dp[i-1][j];
+                    int op2= dp[i][j-1];
+                    dp[i][j]= max(op1, op2);
+                }
+            }
+        }
+    int result = n-dp[n][m];
+    cout<<result<<endl;
+    }
+    return 0;
+}
